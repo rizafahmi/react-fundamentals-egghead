29d8bdb37edcde45dc84040cb5960a6aafbbaf1e
Riza Fahmi <rizafahmi@gmail.com>
2016-08-06 18:41:58 +0700

    Introduction to properties
    https://egghead.io/lessons/react-introduction-to-properties

--- App.js
+++ App.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// class App extends React.Component {
-//   render() {
-//     return <h1>Hi</h1>
-//   }
-// }
+class App extends React.Component {
+  render() {
+    let txt = this.props.txt
+    let cat = this.props.cat
+    return <h1>{cat} - {txt}</h1>
+  }
+}
+
+App.defaultProps = {
+  txt: "This is default text"
+}
+
+App.propTypes = {
+  txt: React.PropTypes.string,
+  cat: React.PropTypes.number.isRequired
+}
 
 // Stateless function
-const App = () => <h1>Hello EggHead</h1>
+// const App = () => <h1>Hello EggHead</h1>
 
-ReactDOM.render(<App />, document.getElementById('app'))
+ReactDOM.render(<App cat={1} />, document.getElementById('app'))
