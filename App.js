3c86077ff913fee32f5eb7cc7434dd966744877f
Riza Fahmi <rizafahmi@gmail.com>
2016-08-21 18:52:00 +0700

    Dinamically Generate Component #14

--- App.js
+++ App.js
@@ -1,47 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-let Mixins = InnerComponent => class extends React.Component {
+class App extends React.Component {
   constructor() {
     super()
-    this.state = {
-      val: 0
-    }
-    this.update = this.update.bind(this)
-  }
-  update() {
-    this.setState({val: this.state.val + 1})
-  }
-  componentWillMount() {
-    console.log('will mount')
-  }
-  componentDidMount() {
-    console.log('did mount')
-  }
-  render() {
-    return <InnerComponent
-      update={this.update}
-      {...this.state}
-      {...this.props} />
+    this.state = {data: [
+      {id: 1, name: "Eric Baily"},
+      {id: 2, name: "Henrik Mickitarian"},
+      {id: 3, name: "Zlatan Ibrahimovic"},
+      {id: 4, name: "Paul Pogba"}
+    ]}
   }
 
+  render() {
+    let rows = this.state.data.map( person => {
+      return <PersonRow key={person.id} data={person} />
+    })
+    return <table>
+      <tbody>{rows}</tbody>
+      </table>
+  }
 }
 
-const Button = (props) => <button onClick={props.update}>{props.txt} - {props.val}</button>
-const Label = (props) => <label onMouseMove={props.update}>{props.txt} - {props.val}</label>
-
-let ButtonMixed = Mixins(Button)
-let LabelMixed = Mixins(Label)
-
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <ButtonMixed txt="Button" />
-        <LabelMixed txt="Label" />
-      </div>
-    )
-  }
+const PersonRow = (props) => {
+  return <tr>
+    <td>{props.data.id}</td>
+    <td>{props.data.name}</td>
+    </tr>
 }
 
 ReactDOM.render(<App />, document.getElementById('app'))
